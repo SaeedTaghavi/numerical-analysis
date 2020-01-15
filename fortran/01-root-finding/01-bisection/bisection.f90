66ee13a83766fbfb1ebf058e9bde3015994d1861
Saeed Taghavi <saeed.taghavi.v@gmail.com>
2020-01-15 20:10:29 +0330

    the fortran version of the bisection changed, I have removed the signf(x) function and instead off sign(x) i have used the f(c)*f(a)>0 as the condition of f(a) and f(c) having the same sign

--- fortran/01-root-finding/01-bisection/bisection.f90
+++ fortran/01-root-finding/01-bisection/bisection.f90
@@ -1,71 +1,58 @@
 program bisection
-    implicit none
-    real(8),parameter:: tol=1e-3            ! tolerance
-    integer, parameter:: max_step=100      ! maximum number of iterations
-    real(8) :: a                            ! left point
-    real(8) :: b                            ! right point
-    real(8) :: c                            ! estimated root
-    integer :: i                            ! index
-    real(8) :: f                            ! we want the root of this function
-    real(8) :: signf                            ! we want the root of this function
+   implicit none
+   real(8), parameter:: tol = 1e-3            ! tolerance
+   integer, parameter:: max_step = 100      ! maximum number of iterations
+   real(8) :: a                            ! left point
+   real(8) :: b                            ! right point
+   real(8) :: c                            ! estimated root
+   integer :: i                            ! index
+   real(8) :: f                            ! we want the root of this function
+   real(8) :: signf                            ! we want the root of this function
 
-    a = -2.0
-    b = 5.0
-    c = 0.0
-    c = (a + b)/2.0                         ! calculate the midpoint
-    i=0
-    
-    ! Display the table header and initial data 
-    write(*,'(A5,6A20)')'i', 'a', 'b', 'c', 'f(a)', 'f(b)', 'f(c)'
-    write(*,'(i5,6F20.5)')i, a, b, c, f(a), f(b), f(c)
-    ! write(*,'(i5,6F20.5)')i, a, b, c, f(a), f(b), f(c) ! for the scientific representation of the number uncomment this line and comment the previous line
+   a = -2.0
+   b = 5.0
+   c = 0.0
+   c = (a + b)/2.0                         ! calculate the midpoint
+   i = 0
 
-    ! Evaluate loop until the result is less than the tolerance 
-    ! maximum number of iterations is not yet reached
+   ! Display the table header and initial data
+   write (*, '(A5,6A20)') 'i', 'a', 'b', 'c', 'f(a)', 'f(b)', 'f(c)'
+   write (*, '(i5,6F20.5)') i, a, b, c, f(a), f(b), f(c)
+   ! write(*,'(i5,6F20.5)')i, a, b, c, f(a), f(b), f(c) ! for the scientific representation of the number uncomment this line and comment the previous line
 
-    if (f(c)==0) then   
-    ! If the first midpoint gives f(c) = 0, c is the root
-        write(*,*) "the root is:", c
-    else 
-        do while( (abs(f(c))>tol) .and. (i<=max_step) )
-            if ( signf(f(c))==signf(f(a)) ) then 
-                ! f(c) has same sign as f(a)
-                a=c
-            else 
-                ! f(c) has same sign as f(b)
-                b=c
-            end if
-            c = (a+b)/2.0       ! midpoint update 
-            i=i+1               ! index increment
-            ! display current data 
-            write(*,'(i5,6F20.5)')i, a, b, c, f(a), f(b), f(c)
-        end do
-    end if
+   ! Evaluate loop until the result is less than the tolerance
+   ! maximum number of iterations is not yet reached
 
-    ! Display results 
-    write(*,*) "root is c=",c, "fount after ", i, "iterations."
-    write(*,*) "the value of the function f(c) is:",f(c)
-    
+   if (f(c) == 0) then
+      ! If the first midpoint gives f(c) = 0, c is the root
+      write (*, *) "the root is:", c
+   else
+      do while ((abs(f(c)) > tol) .and. (i <= max_step))
+         if (f(c)*f(a)>0.0) then
+            ! f(c) has same sign as f(a)
+            a = c
+         else
+            ! f(c) has same sign as f(b)
+            b = c
+         end if
+         c = (a + b)/2.0       ! midpoint update
+         i = i + 1               ! index increment
+         ! display current data
+         write (*, '(i5,6F20.5)') i, a, b, c, f(a), f(b), f(c)
+      end do
+   end if
+
+   ! Display results
+   write (*, *) "root is c=", c, "fount after ", i, "iterations."
+   write (*, *) "the value of the function f(c) is:", f(c)
+
 end program bisection
 
-! function definition 
+! function definition
 function f(x)
-    implicit none
-    real(8)::x
-    real(8)::f
-    f = 10.0 - x*x;
-end function f 
+   implicit none
+   real(8)::x
+   real(8)::f
+   f = 10.0 - x*x; 
+end function f
 
-! sign function is needed in the implimentation
-function signf(x)
-    implicit none
-    real(8):: x
-    real(8) :: signf
-    if (x>0) then 
-        signf = +1.0
-    else if (x<0) then 
-        signf = -1.0
-    else if (x==0) then 
-        signf = 0.0
-    end if
-end function signf
