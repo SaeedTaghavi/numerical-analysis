703a0d4fc301ed47336299d4851a763ab17d1d18
saeedtaghavi <saeed.taghavi.v@gmail.com>
2020-01-20 17:04:39 +0330

    code is NOT compelete yet

--- fortran/01-root-finding/03-fixed-point/README.md
+++ fortran/01-root-finding/03-fixed-point/README.md
@@ -2,6 +2,11 @@
 
 # fixed-point method
 
+- [ ] finish the code
+- [ ] compelete the documentation-find a way other than the github readme, because github does not render latex for the mathematics
+- [ ] Push my commits to GitHub
+
+
 Suppose that the equation f(x) = 0 is written in the form x = g(x); that is,
 
 f(x) = x − g(x) = 0.
@@ -38,13 +43,21 @@ will converge to the root x = ξ for any choice of the initial approximation x_0
 The simplest way to write f(x) = 0 in the form x = g(x) is to add $x$ on both sides, that is,
 x = f(x) + x = g(x).
 But it does not very often work.
-
-
+the goal of this code is to find the root of the 
+f(x) = x\*\*3 − 6\*x\*\*2 + 11\*x − 6 = 0
+with the fixed point iteration.
+we define g(x) = x + f(x) = x\*\*3 − 6\*x\*\*2 + 12\*x − 6
+We know that there is a root of f(x) in [2.5, 4]; namely x = 3.
+Let’s start the iteration xk+1 = g(xk) with x0 = 3.5.
+Then we have:
+x1 = g(x0) = g(3.5) = 5.3750,
+x2 = g(x1) = g(5.3750) = 40.4434,
+x3 = g(x2) = g(40.4434) = 5.6817 × 10\*\*4,
+and x4 = g(x3) = g(5.6817 × 10\*\*4) = 1.8340 × 10\*\*14
+.
+The sequence {xk} is clearly diverging
 
 You can find more about the fixed-point method [here](https://en.wikipedia.org/wiki/Fixed-point_iteration).
 There are some plots, flowchart and animation that are interesting and helpful.
 
 
-- [ ] finish the code
-- [ ] compelete the documentation-find a way other than the github readme, because github does not render latex for the mathematics
-- [ ] Push my commits to GitHub
--- fortran/01-root-finding/03-fixed-point/fixed-point.f90
+++ fortran/01-root-finding/03-fixed-point/fixed-point.f90
@@ -3,60 +3,53 @@ program fixedpoint
         !!! the goal of this code is to find the root of the 
         !!! f(x) = x**3 − 6*x**2 + 11*x − 6 = 0
         !!! with the fixed point iteration.
+        !!! x =1/11 (−x**3 + 6*x**2 + 6) = g(x)
+        !!! Then 
+        !!! (i) for all x ∈ [2.5, 4], g(x) ∈ [2.5, 4] (Note that g(2.5) = 2.5341 and g(4) = 3.4545).
+        !!! (ii) g'(x) = 1/11 (−3*x**2 + 12*x) steadily decreases in [2.5, 4] and remains less than 1 for all x in (2.5, 4). (Note that 
+        !!! g′(2.5) = 1.0227 and g′(4) = 0.)
+        !!! Thus, both hypotheses of Theorem 1.6 are satisfied. So, according to this theorem, for any x0 in [2.5, 4], 
+        !!! the sequence {xk} should converge to the fixed point.
+        !!!
+        !!!
+
     implicit none
     real(8),parameter:: tol=1e-3            ! tolerance
-    integer, parameter:: max_step=100      ! maximum number of iterations
+    integer, parameter:: max_step=10       ! maximum number of iterations
     real(8) :: a                            ! left point
     real(8) :: b                            ! right point
-    real(8) :: c                            ! estimated root
-    integer :: i                            ! index
     real(8) :: f                            ! we want the root of this function
-    real(8) :: signf                            ! we want the root of this function
-
-    a = -2.0
-    b = 5.0
-    c = 0.0
-    c = (a + b)/2.0                         ! calculate the midpoint
-    i=0
-    
+    real(8) :: g                            ! 
+    real(8) :: x,temp                            ! the root
+    integer :: i                            ! index
+    a = 2.5
+    b = 4.0
+    x=3.5
+    i = 0
     ! Display the table header and initial data 
-    write(*,'(A5,6A20)')'i', 'a', 'b', 'c', 'f(a)', 'f(b)', 'f(c)'
-    write(*,'(i5,6F20.5)')i, a, b, c, f(a), f(b), f(c)
+    write(*,'(A5,4A20)')'i', 'x(i)' , 'g(x(i))', 'x(i+1)', 'f(x(i+1))'
+    write(*,'(i5,4F20.5)')i, x, b, c, f(a), f(b), f(c)
     ! write(*,'(i5,6F20.5)')i, a, b, c, f(a), f(b), f(c) ! for the scientific representation of the number uncomment this line and comment the previous line
 
     ! Evaluate loop until the result is less than the tolerance 
     ! maximum number of iterations is not yet reached
 
-    if (f(c)==0) then   
-    ! If the first midpoint gives f(c) = 0, c is the root
-        write(*,*) "the root is:", c
-    else 
-        do while( (abs(f(c))>tol) .and. (i<=max_step) )
-            if ( signf(f(c))==signf(f(a)) ) then 
-                ! f(c) has same sign as f(a)
-                a=c
-            else 
-                ! f(c) has same sign as f(b)
-                b=c
-            end if
-            c = (a+b)/2.0       ! midpoint update 
-            i=i+1               ! index increment
-            ! display current data 
-            write(*,'(i5,6F20.5)')i, a, b, c, f(a), f(b), f(c)
-        end do
-    end if
+    do i =1,max_step
+        x=g(x)
+        print*,x
+    end do
 
     ! Display results 
-    write(*,*) "root is c=",c, "fount after ", i, "iterations."
-    write(*,*) "the value of the function f(c) is:",f(c)
+!    write(*,*) "root is c=",c, "fount after ", i, "iterations."
+!    write(*,*) "the value of the function f(c) is:",f(c)
     
 end program fixedpoint
 
 ! function definition 
-function f(x)
+function g(x)
     implicit none
     real(8)::x
-    real(8)::f
-    f = 10.0 - x*x;
-end function f 
+    real(8)::g
+    g =(-x*x*x+6.0*x*x+6.0)/11.0
+end function g
 
